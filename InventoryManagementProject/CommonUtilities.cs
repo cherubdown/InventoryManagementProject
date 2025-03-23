443e1d68c7612505393dcd7b21e82438b27489af
Christopher Bales <radagast1986@yahoo.com>
2025-03-23 00:25:32 -0500

    Common Utilities for IO

---
+++ InventoryManagementProject/CommonUtilities.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagementProject
+{
+    public static class CommonUtilities
+    {
+        /// <summary>
+        /// Asks the user for a common Y/N and converts to boolean (Yes is true, No is false)
+        /// If the user does not press Y or N (or y or n) this function will loop until they do
+        /// Exiting out of this is not an option
+        /// </summary>
+        /// <returns>A boolean that represents Yes or No</returns>
+        public static bool ReadUserKeyAndDetermineYesOrNo()
+        {
+            char keyPressed = Console.ReadKey().KeyChar;
+            if (keyPressed == 'y')
+            {
+                return true;
+            }
+            else if (keyPressed == 'n')
+            {
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("That is not a valid response. Please enter Y or N (or y or n) for Yes or No respectively.");
+                return ReadUserKeyAndDetermineYesOrNo();
+            }
+        }
+    }
+}
--- InventoryManagementProject/InventoryManager.cs
+++ InventoryManagementProject/InventoryManager.cs
@@ -17,11 +17,12 @@ namespace InventoryManagementProject
         public void Begin()
         {
             Products = DataWriter.LoadDatabase();
+            Console.WriteLine("Welcome to the Inventory Management System. What do you want to do?");
 
-            while(true)
+            while (true)
             {
-                Console.WriteLine(Environment.NewLine);
-                Console.WriteLine("Welcome to the Inventory Management System. What do you want to do?");
+
+                Console.WriteLine(Environment.NewLine + "Options:" + Environment.NewLine);
                 Console.WriteLine("1. View products");
                 Console.WriteLine("2. Add new product");
                 Console.WriteLine("3. Sell a product");
@@ -58,20 +59,18 @@ namespace InventoryManagementProject
         {
             foreach(ConcreteProduct product in Products)
             {
-                WriteProductToConsole(product);
+                WriteProductDetailsRowToConsole(product);
             }
         }
 
-        private void WriteProductToConsole(ConcreteProduct product)
+        private void WriteProductDetailsRowToConsole(ConcreteProduct product)
         {
             Console.WriteLine($"Product name: {product.Name}, price: {product.Price}, stock: {product.Stock}");
         }
 
-        
-
         private void AddProductWithInputValidation()
         {
-            Console.WriteLine("What is the name of the product?:");
+            Console.WriteLine("Add Product: What is the name of the product?:");
             string? name = Console.ReadLine();
             if (name == null)
             {
@@ -79,7 +78,7 @@ namespace InventoryManagementProject
                 return;
             }
 
-            Console.WriteLine("What is the price of the product?:");
+            Console.WriteLine("Add Product: What is the price of the product?:");
             string? priceStr = Console.ReadLine();
             double price;
             if (!double.TryParse(priceStr, out price))
@@ -88,7 +87,7 @@ namespace InventoryManagementProject
                 return;
             }
 
-            Console.WriteLine("How many do you have in stock?:");
+            Console.WriteLine("Add Product: How many do you have in stock?:");
             string? stockStr = Console.ReadLine();
             int stock;
             if (!int.TryParse(stockStr, out stock) && stock >= 0)
@@ -96,30 +95,61 @@ namespace InventoryManagementProject
                 Console.WriteLine("Invalid stock price. It should represent a positive integer or zero. Item not added.");
                 return;
             }
+
             AddProduct(new ConcreteProduct(name, price, stock));
         }
 
-        public void AddProduct(ConcreteProduct product)
+        private void AddProduct(ConcreteProduct product)
         {
             if (FindProductByName(product.Name) == null)
             {
                 Products.Add(product);
-                WriteProductToConsole(product);
+                WriteProductDetailsRowToConsole(product);
             } 
             else
             {
-                Console.WriteLine("That product already exists.");
+                Console.WriteLine("ERROR: That product already exists.");
             }
         }
 
-        public ConcreteProduct? FindProductByName(string name)
+        private ConcreteProduct? FindProductByName(string name)
         {
-            return Products.Find(p => p.Name == name);
+            return Products.Find(p => p.Name.ToLowerInvariant() == name.ToLowerInvariant());
         }
 
-        public void SellProductByName(string name, int quantity)
+        private void SellProductByName(string name, int quantity)
         {
+            // find the product and return if it doesn't exist
             ConcreteProduct? product = FindProductByName(name);
+            if (product == null)
+            {
+                Console.WriteLine($"Product '{name}' does not exist.");
+                Console.WriteLine($"Would you like to add it to inventory? Y/N");
+                if (CommonUtilities.ReadUserKeyAndDetermineYesOrNo())
+                {
+
+                }
+                return;
+            }
+
+            // cannot have a negative quantity of stock
+            if (product.Stock > quantity)
+            {
+                
+                Console.WriteLine($"Product '{name}' has only {product.Stock} in stock. You cannot sell more than what is in stock.");
+                Console.WriteLine($"Would you like to sell all {product.Stock} instead? Y/N");
+
+                // if user would like to sell the entire stock, call the function again with the known stock amount
+                // otherwise kick out of process
+                if (CommonUtilities.ReadUserKeyAndDetermineYesOrNo())
+                {
+                    SellProductByName(name, product.Stock);
+                }
+                else
+                {
+                    return;
+                }
+            }
 
         }
     }
